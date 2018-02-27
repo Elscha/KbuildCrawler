d8e97bdc911b4ddd4b615e6b63c5d3a55785a9d9
adam-sse <adam@sse.uni-hildesheim.de>
2018-02-27 15:04:25 +0100

    Remove unneeded classes

--- KbuildCrawler/test/net/ssehub/kBuildCrawler/git/AllGitTests.java
+++ KbuildCrawler/test/net/ssehub/kBuildCrawler/git/AllGitTests.java
@@ -4,13 +4,12 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import net.ssehub.kBuildCrawler.git.diff.DiffUtilsTests;
 import net.ssehub.kBuildCrawler.git.mail_parsing.GitUtilsTest;
 
 @RunWith(Suite.class)
 @SuiteClasses({
-    DiffUtilsTests.class,
-    GitUtilsTest.class })
+    GitUtilsTest.class,
+    })
 public class AllGitTests {
 
 }
