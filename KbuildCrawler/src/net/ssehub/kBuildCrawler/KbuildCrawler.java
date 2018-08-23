812ba0c018732ebaca6d5f0ec36d19f166553e7d
adam-sse <adam@sse.uni-hildesheim.de>
2018-08-23 13:02:35 +0200

    Remove temporary debug measure again

--- KbuildCrawler/src/net/ssehub/kBuildCrawler/KbuildCrawler.java
+++ KbuildCrawler/src/net/ssehub/kBuildCrawler/KbuildCrawler.java
@@ -32,7 +32,7 @@ public class KbuildCrawler {
     public final static File TESTDATA = new File("testdata");
     private static final File TEST_ARCHIVE = new File(TESTDATA, "2016-August.txt.gz");
     
-    private static final int DEBUG_PROCESS_ONLY = 1;
+    private static final int DEBUG_PROCESS_ONLY = -1;
     
     public static final boolean DISABLE_KH_LOGGING = true;
 
