9d9f7fbef186a8cef66754b6e3ffbbdf41b5dd78
adam-sse <adam@sse.uni-hildesheim.de>
2018-09-20 11:58:36 +0200

    Set DEBUG_PROCESS_ONLY to 2 (temporarily)

--- KbuildCrawler/src/net/ssehub/kBuildCrawler/KbuildCrawler.java
+++ KbuildCrawler/src/net/ssehub/kBuildCrawler/KbuildCrawler.java
@@ -41,7 +41,7 @@ public class KbuildCrawler {
     public final static File TESTDATA = new File("testdata");
     private static final File TEST_ARCHIVE = new File(TESTDATA, "2016-August.txt.gz");
     
-    private static final int DEBUG_PROCESS_ONLY = -1;
+    private static final int DEBUG_PROCESS_ONLY = 2;
     
     public static final boolean DISABLE_KH_LOGGING = true;
 
